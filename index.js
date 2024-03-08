c3e778c79736137659c38304f2f8047b21ce0ddf
Karthik <karthik.krishnaswamy@ratnaglobaltech.com>
2024-03-08 12:40:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Sample data for the API
+const data = {
+  message: 'Hello from the backend API! kkkk',
+  
+};
+
+// Define the API endpoint
+app.get('/api/data', (req, res) => {
+  res.json(data);
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
